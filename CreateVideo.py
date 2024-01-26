e80a5948b57f3c97bfc036df6ed5462079064285
Tulpin9210 <121322076+Tulpin9210@users.noreply.github.com>
2024-01-26 18:40:37 -0500

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg', '.jfif']:
+        file_name = path+"/"+file
+
+        #imprime(file_name)
+
+        images.append(file_name)
+
+#imprime(len(images))
+count = len(images)
+
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size = (width, height)
+#imprime size
+
+
+out = cv2.VideoWriter('project.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+
+#PARA EL ATARDECER
+#for i in range(0, count-1):
+
+#PARA EL AMANECER
+for i in range(count-1,0,-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+
+out.release() # liberar el video generado
+print("Listo")
+
+
+
